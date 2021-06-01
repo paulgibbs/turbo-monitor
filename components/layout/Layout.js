c871469c642f1e460cafa8b557523358bd5fa3fb
Christopher Geary <hello@crgeary.com>
2021-06-01 18:44:26 +0100

    Add webappmanifest

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -8,6 +8,12 @@ export const Layout = ({ children }) => (
         <Head>
             <title>Turbo</title>
             <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+            <link rel="manifest" href="/manifest.webmanifest" />
+            <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+            <meta name="theme-color" content="#1400ff" />
+
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
         </Head>
 
